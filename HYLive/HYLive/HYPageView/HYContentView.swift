c856e9b9cda90ada2351a7a3039125367d03ef66
ALittleNasty <379643671@qq.com>
2017-06-27 13:47:41 +0800

    添加HYPageView

---
+++ HYLive/HYLive/HYPageView/HYContentView.swift
@@ -0,0 +1,21 @@
+//
+//  HYContentView.swift
+//  HYLive
+//
+//  Created by ALittleNasty on 2017/6/27.
+//  Copyright © 2017年 ALittleNasty. All rights reserved.
+//
+
+import UIKit
+
+class HYContentView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ HYLive/HYLive/HYPageView/HYPageStyle.swift
@@ -0,0 +1,13 @@
+//
+//  HYPageStyle.swift
+//  HYLive
+//
+//  Created by ALittleNasty on 2017/6/27.
+//  Copyright © 2017年 ALittleNasty. All rights reserved.
+//
+
+import UIKit
+
+class HYPageStyle {
+
+}
