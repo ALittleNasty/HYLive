909d51e0c8b40b82f37623e5942f01d9e1ff4f7c
ALittleNasty <379643671@qq.com>
2017-07-17 15:26:26 +0800

    hook系统的UINavigationController的方法, 实现全屏手势pop

---
+++ HYLive/HYLive/HYNavigationController.swift
@@ -0,0 +1,60 @@
+//
+//  HYNavigationController.swift
+//  HYLive
+//
+//  Created by ALittleNasty on 2017/7/17.
+//  Copyright © 2017年 ALittleNasty. All rights reserved.
+//
+
+import UIKit
+
+class HYNavigationController: UINavigationController {
+
+    // 重写preferredStatusBarStyle这个属性, 改变状态栏为白色
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // 校验这个手势是否存在
+        guard let popGesture = interactivePopGestureRecognizer else {
+            return
+        }
+        
+        /* 通过运行时机制回去所有属性
+        var count: UInt32 = 0
+        let ivars = class_copyIvarList(UIGestureRecognizer.self, &count)!
+        print("Ivar List count: \(count)")
+        for i in 0..<count {
+            let ivar = ivars[Int(i)]
+            let namePointer = ivar_getName(ivar)
+            let name = String(cString: namePointer!)
+            print(name)
+        } */
+        
+        // action=handleNavigationTransition:, target=<_UINavigationInteractiveTransition 0x7fd947600980>
+        guard let targets = popGesture.value(forKeyPath: "_targets") as? [NSObject] else {
+            return
+        }
+        
+        guard let obj = targets.first else {
+            return
+        }
+        
+        let action = Selector(("handleNavigationTransition:")) // action通过KVC获取不到, 所以只能通过它响应的方法生成一个Selector
+        let target = obj.value(forKeyPath: "target")           // target可通过KVC直接获取
+        
+        // 创建自己的手势
+        let panGesture = UIPanGestureRecognizer(target: target, action: action)
+        
+        view.addGestureRecognizer(panGesture)
+    }
+    
+    
+    override func pushViewController(_ viewController: UIViewController, animated: Bool) {
+        viewController.hidesBottomBarWhenPushed = true
+        super.pushViewController(viewController, animated: animated)
+    }
+}
