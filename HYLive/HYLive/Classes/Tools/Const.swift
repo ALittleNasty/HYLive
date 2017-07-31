738c62f1aee0f910d1bbc42f034bf417283873f7
ALittleNasty <379643671@qq.com>
2017-07-31 16:58:44 +0800

    添加资源文件

---
+++ HYLive/HYLive/Classes/Tools/Const.swift
@@ -0,0 +1,16 @@
+//
+//  Const.swift
+//  HYLive
+//
+//  Created by ALittleNasty on 2017/7/31.
+//  Copyright © 2017年 ALittleNasty. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let kScreenWidth = UIScreen.main.bounds.width
+let kScreenHeight = UIScreen.main.bounds.height
+let kNavigationBarHeight : CGFloat = 44
+let kStatusBarHeight : CGFloat = 44
+
